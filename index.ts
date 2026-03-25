9873716968f456c16031072e3e3c9eeedd41d639
Ninepiaths1 <thezore21@gmail.com>
2026-03-25 20:29:35 +0800

    Update index.ts

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ app.disable('x-powered-by');
 // ================= MIDDLEWARE =================
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(express.json());
 
 const limiter = rateLimit({
   windowMs: 60_000,
